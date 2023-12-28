1e08e8a4738ad9485b2f5e3bf9c78ee3d4ad9ad9
Muruvvetbati <batimuruvvet@gmail.com>
2023-12-29 00:06:40 +0300

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Devmuruvvet on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
